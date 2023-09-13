1552329f721c9cde0d1e413918aac508cdac4f42
Fei Wen <138689471+FWkey@users.noreply.github.com>
2023-09-13 11:40:45 +0800

    Update linear.h

--- torch_int/kernels/include/linear.h
+++ torch_int/kernels/include/linear.h
@@ -2,42 +2,22 @@
 #define LINEAR_H
 #include <torch/types.h>
 
-// used by out_proj and fc2, return INT32
-torch::Tensor linear_a8_w8_b32_o32(torch::Tensor input,  // INT8
-                                   torch::Tensor weight, // INT8
-                                   torch::Tensor bias    // INT32
+torch::Tensor linear_a8_w8_bbf16_obf16(torch::Tensor input,  // INT8
+                                       torch::Tensor weight, // INT8
+                                       torch::Tensor bias,   // BF16
+                                      float alpha  // 
 );
 
-// used by out_proj and fc2, return INT32
-torch::Tensor linear_a8_w8_b32_o32_with_scaling(torch::Tensor input,  // INT8
-                                                torch::Tensor weight, // INT8
-                                                torch::Tensor bias,   // INT32
-                                                float alpha,          // FP32
-                                                float beta            // FP32
+torch::Tensor linear_a8_w8_bfp16_ofp16(torch::Tensor input,  // INT8
+                                       torch::Tensor weight, // INT8
+                                       torch::Tensor bias,   // FP16
+                                      float alpha  // 
 );
 
-// used by out_proj and fc2, return FP32
 torch::Tensor linear_a8_w8_bfp32_ofp32(torch::Tensor input,  // INT8
                                        torch::Tensor weight, // INT8
                                        torch::Tensor bias,   // FP32
-                                       float alpha,          // FP32
-                                       float beta            // FP32
-);
-
-// used by fc1, return INT8
-torch::Tensor linear_relu_a8_w8_b8_o8(torch::Tensor input,  // INT8
-                                      torch::Tensor weight, // INT8
-                                      torch::Tensor bias,   // INT8
-                                      float alpha,          // FP32
-                                      float beta            // FP32
-);
-
-// used by q_proj, k_proj, v_proj, return INT8
-torch::Tensor linear_a8_w8_b8_o8(torch::Tensor input,  // INT8
-                                 torch::Tensor weight, // INT8
-                                 torch::Tensor bias,   // INT8
-                                 float alpha,          // FP32
-                                 float beta            // FP32
+                                      float alpha  // 
 );
 
 #endif // LINEAR_HS
